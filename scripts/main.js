1fe55fd3c217ee6460c73ee620b2bcc2e74b5dd2
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-09-21 16:46:41 -0600

    dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -33,6 +33,64 @@ async function ConsoleLogPortfolio() {
 }
 ConsoleLogPortfolio();
 
+// Select the node that will be observed for mutations
+const targetNode = document.querySelector("nav");
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+      darkMode();
+      observer.disconnect();
+    } else if (mutation.type === "attributes") {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+if (window.localStorage.getItem('darkmode') == 'TRUE') {
+    let element = document.querySelector("body");
+
+    element.classList.remove('light');
+    element.classList.remove('dark');
+    element.classList.add('dark');
+    document.getElementById('theme-btn').innerHTML = 'dark_mode';
+}
+
+async function darkMode() {
+    let element = document.querySelector("body");
+
+    const themeBtn = document.getElementById("theme-btn");
+    if (window.localStorage.getItem('darkmode') == 'TRUE') {
+        element.classList.remove('light');
+        element.classList.add('dark');
+        themeBtn.innerHTML = 'light_mode';
+    }
+    themeBtn.onclick = () => {
+        if (themeBtn.innerHTML === 'dark_mode') {
+            element.classList.remove('light');
+            element.classList.add("dark");
+            themeBtn.innerHTML = 'light_mode';
+            window.localStorage.setItem('darkmode', 'TRUE');
+        } else {
+            element.classList.remove('dark');
+            element.classList.add("light");
+            themeBtn.innerHTML = 'dark_mode';
+            window.localStorage.setItem('darkmode', 'FALSE');
+        }
+    }
+}
+
 
 
 const parallax = document.querySelector('main');
